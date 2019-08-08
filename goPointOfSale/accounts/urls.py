237ddf7227e9970a7d5c7da9542afba669875060
sabareesh <sabareesh.madambath@gmail.com>
2019-08-09 01:20:46 +0530

    Committing Travello - After Adding DB Details2

--- goPointOfSale/accounts/urls.py
+++ goPointOfSale/accounts/urls.py
@@ -14,14 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('travello.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('register',views.register, name='register'),
 ]
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
